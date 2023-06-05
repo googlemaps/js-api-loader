559dc7e5e0f74380acf12d9a8e0faa6f5510cede
Alex Muramoto <amuramoto@users.noreply.github.com>
2023-06-05 12:43:51 -0700

    chore: Update jest.config.js
    lowering the test thresholds to something more reasonable.

--- jest.config.js
+++ jest.config.js
@@ -21,10 +21,10 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
