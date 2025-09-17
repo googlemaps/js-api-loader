c86821d59f2c67292b6102f3d6664d1ee0ecdd44
Martin Schuhfuss <m.schuhfuss@gmail.com>
2025-09-17 11:00:00 +0200

    fix: make Loader stub fail hard

--- src/deprecated.ts
+++ src/deprecated.ts
@@ -4,50 +4,14 @@
  */
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function */
 
-import { ERR_DEPRECATED_LOADER, logErrorOnce } from "./messages.js";
+import { ERR_DEPRECATED_LOADER } from "./messages.js";
 
 /**
  * @deprecated Use the new functional API: `setOptions()` and `importLibrary()`.
  * See the migration guide for more details: MIGRATION.md
  */
 export class Loader {
-  /** @deprecated */
-  status = 0;
-
-  /** @deprecated */
-  options = {};
-
   constructor(...args: any[]) {
-    logErrorOnce(ERR_DEPRECATED_LOADER);
-  }
-
-  /** @deprecated */
-  public createUrl(): string {
-    return "";
-  }
-
-  /** @deprecated */
-  public deleteScript(): void {
-    // do nothing
-  }
-
-  /** @deprecated */
-  public async load(): Promise<any> {
-    return new Promise(() => {});
-  }
-
-  /** @deprecated */
-  public async loadPromise(): Promise<any> {
-    return this.load();
-  }
-
-  /** @deprecated */
-  public importLibrary(libraryName: any): Promise<any> {
-    return new Promise(() => {});
-  }
-
-  /** @deprecated */
-  public loadCallback(callback: any): void {
-    // do nothing
+    throw new Error(`[@googlemaps/js-api-loader]: ${ERR_DEPRECATED_LOADER}`);
   }
 }
