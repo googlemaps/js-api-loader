7064109ec19e623a536d92ee3dc7be3bc91f9234
Martin Schuhfuss <m.schuhfuss@gmail.com>
2025-09-22 18:11:43 +0200

    fix: add docs for bootstrap function

--- src/bootstrap.d.ts
+++ src/bootstrap.d.ts
@@ -5,4 +5,18 @@
 
 import { APIOptions } from "./index.js";
 
+/**
+ * Initializes and configures the Google Maps JavaScript API.
+ *
+ * When called the first time, this will install a proxy for the
+ * `google.maps.importLibrary` function that will load the Maps JavaScript
+ * API when called for the first time. Once the API is loaded, the proxy
+ * function will be replaced with the real implementation.
+ *
+ * Any call beyond that will just trigger a warning being logged to the console.
+ *
+ * @function bootstrap
+ * @param options - The configuration options to initialize the application.
+ * @returns {void}
+ */
 export const bootstrap: (options: APIOptions) => void;
